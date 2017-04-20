77a14343cbf989dc93251c7016710efcbf671542
Pradeep Prabhu Bellikoth Ramesh <pbellikoth001@nam.ad.pwcinternal.com>
2017-04-20 17:43:41 +0530

    basic local storage and alert prompt

--- todo/src/pages/home/home.ts
+++ todo/src/pages/home/home.ts
@@ -8,8 +8,11 @@ import { AlertController } from 'ionic-angular'
 export class HelloIonicPage {
   tasks: any;
   quickToDo: string;
-  constructor(private settingService: SettingsService,private alertCtrl:AlertController) {
-    this.tasks = [{ name: "pradeep" }, { name: "prabhu" }];
+  noClicked: boolean = false;
+  constructor(private settingService: SettingsService, private alertCtrl: AlertController) {
+    var storageList = localStorage.getItem("taskList");
+    console.log("storage",storageList);
+    this.tasks = (storageList==null) ?[]: JSON.parse(storageList);
     this.quickToDo = ""
   }
   change(event, task) {
@@ -17,33 +20,51 @@ export class HelloIonicPage {
     var index = this.tasks.indexOf(task);
     console.log(index)
     console.log(event)
-    if (this.settingService.alertOnDelete === true) {
-      let confirm = this.alertCtrl.create({
-        title: 'Delete Task?',
-        message: 'Do you want to delete the task?',
-        buttons: [
-          {
-            text: 'No',
-            handler: () => {
-              console.log('No clicked');
+    if (this.noClicked === false) {
+      if (this.settingService.alertOnDelete === true) {
+        let confirm = this.alertCtrl.create({
+          title: 'Delete Task?',
+          message: 'Do you want to delete the task?',
+          buttons: [
+            {
+              text: 'No',
+              handler: () => {
+
+                this.noClicked = true;
+                event.checked = false;
+              }
+            },
+            {
+              text: 'Yes',
+              handler: () => {
+                this.tasks.splice(index, 1);
+              }
             }
-          },
-          {
-            text: 'Yes',
-            handler: () => {
-               this.tasks.splice(index, 1);
-            }
-          }
-        ]
-      });
-      confirm.present();
+          ]
+        });
+        confirm.present();
+      }
+      else {
+        this.tasks.splice(index, 1);
+      }
     }
-    else{
-    this.tasks.splice(index, 1);
+    else {
+      this.noClicked = false;
     }
   }
   createTodo() {
-    var task = { name: this.quickToDo };
-    this.tasks.push(task);
+    if (this.quickToDo.trim() === "") {      
+      let alertBox = this.alertCtrl.create({
+        title: 'Empty Task',
+        subTitle: 'Please add a title to create a task?',
+        buttons: ['OK']
+      });
+      alertBox.present();
+    }
+    else {
+      var task = { name: this.quickToDo };
+      this.tasks.push(task);
+      localStorage.setItem("taskList",  JSON.stringify(this.tasks));
+    }
   }
 }
